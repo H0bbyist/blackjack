ded893235ea47592dc175250bcafca839e9fb184
Jesse Weaver <jesse354@gmail.com>
2018-03-05 10:48:11 -0600

    formatting

--- blackjack.js
+++ blackjack.js
@@ -1,6 +1,6 @@
 console.log("Sup");
 
-var deal =document.getElementById("deal-button")
+var deal = document.getElementById("deal-button")
 var hit = document.getElementById("hit-button")
 var stand = document.getElementById("stand-button")
 var again = document.getElementById("again-button")
@@ -67,50 +67,154 @@ var deck = [
     { case: "J", suit: 'S', point: 10 },
     { case: "Q", suit: 'S', point: 10 },
     { case: "K", suit: 'S', point: 10 },
+    { case: "A", suit: 'H', point: 11 },
+    { case: 2, suit: 'H', point: 2 },
+    { case: 3, suit: 'H', point: 3 },
+    { case: 4, suit: 'H', point: 4 },
+    { case: 5, suit: 'H', point: 5 },
+    { case: 6, suit: 'H', point: 6 },
+    { case: 7, suit: 'H', point: 7 },
+    { case: 8, suit: 'H', point: 8 },
+    { case: 9, suit: 'H', point: 9 },
+    { case: 10, suit: 'H', point: 10 },
+    { case: "J", suit: 'H', point: 10 },
+    { case: "Q", suit: 'H', point: 10 },
+    { case: "K", suit: 'H', point: 10 },
+    { case: "A", suit: 'D', point: 11 },
+    { case: 2, suit: 'D', point: 2 },
+    { case: 3, suit: 'D', point: 3 },
+    { case: 4, suit: 'D', point: 4 },
+    { case: 5, suit: 'D', point: 5 },
+    { case: 6, suit: 'D', point: 6 },
+    { case: 7, suit: 'D', point: 7 },
+    { case: 8, suit: 'D', point: 8 },
+    { case: 9, suit: 'D', point: 9 },
+    { case: 10, suit: 'D', point: 10 },
+    { case: "J", suit: 'D', point: 10 },
+    { case: "Q", suit: 'D', point: 10 },
+    { case: "K", suit: 'D', point: 10 },
+    { case: "A", suit: 'C', point: 11 },
+    { case: 2, suit: 'C', point: 2 },
+    { case: 3, suit: 'C', point: 3 },
+    { case: 4, suit: 'C', point: 4 },
+    { case: 5, suit: 'C', point: 5 },
+    { case: 6, suit: 'C', point: 6 },
+    { case: 7, suit: 'C', point: 7 },
+    { case: 8, suit: 'C', point: 8 },
+    { case: 9, suit: 'C', point: 9 },
+    { case: 10, suit: 'C', point: 10 },
+    { case: "J", suit: 'C', point: 10 },
+    { case: "Q", suit: 'C', point: 10 },
+    { case: "K", suit: 'C', point: 10 },
+    { case: "A", suit: 'S', point: 11 },
+    { case: 2, suit: 'S', point: 2 },
+    { case: 3, suit: 'S', point: 3 },
+    { case: 4, suit: 'S', point: 4 },
+    { case: 5, suit: 'S', point: 5 },
+    { case: 6, suit: 'S', point: 6 },
+    { case: 7, suit: 'S', point: 7 },
+    { case: 8, suit: 'S', point: 8 },
+    { case: 9, suit: 'S', point: 9 },
+    { case: 10, suit: 'S', point: 10 },
+    { case: "J", suit: 'S', point: 10 },
+    { case: "Q", suit: 'S', point: 10 },
+    { case: "K", suit: 'S', point: 10 },
+    { case: "A", suit: 'H', point: 11 },
+    { case: 2, suit: 'H', point: 2 },
+    { case: 3, suit: 'H', point: 3 },
+    { case: 4, suit: 'H', point: 4 },
+    { case: 5, suit: 'H', point: 5 },
+    { case: 6, suit: 'H', point: 6 },
+    { case: 7, suit: 'H', point: 7 },
+    { case: 8, suit: 'H', point: 8 },
+    { case: 9, suit: 'H', point: 9 },
+    { case: 10, suit: 'H', point: 10 },
+    { case: "J", suit: 'H', point: 10 },
+    { case: "Q", suit: 'H', point: 10 },
+    { case: "K", suit: 'H', point: 10 },
+    { case: "A", suit: 'D', point: 11 },
+    { case: 2, suit: 'D', point: 2 },
+    { case: 3, suit: 'D', point: 3 },
+    { case: 4, suit: 'D', point: 4 },
+    { case: 5, suit: 'D', point: 5 },
+    { case: 6, suit: 'D', point: 6 },
+    { case: 7, suit: 'D', point: 7 },
+    { case: 8, suit: 'D', point: 8 },
+    { case: 9, suit: 'D', point: 9 },
+    { case: 10, suit: 'D', point: 10 },
+    { case: "J", suit: 'D', point: 10 },
+    { case: "Q", suit: 'D', point: 10 },
+    { case: "K", suit: 'D', point: 10 },
+    { case: "A", suit: 'C', point: 11 },
+    { case: 2, suit: 'C', point: 2 },
+    { case: 3, suit: 'C', point: 3 },
+    { case: 4, suit: 'C', point: 4 },
+    { case: 5, suit: 'C', point: 5 },
+    { case: 6, suit: 'C', point: 6 },
+    { case: 7, suit: 'C', point: 7 },
+    { case: 8, suit: 'C', point: 8 },
+    { case: 9, suit: 'C', point: 9 },
+    { case: 10, suit: 'C', point: 10 },
+    { case: "J", suit: 'C', point: 10 },
+    { case: "Q", suit: 'C', point: 10 },
+    { case: "K", suit: 'C', point: 10 },
+    { case: "A", suit: 'S', point: 11 },
+    { case: 2, suit: 'S', point: 2 },
+    { case: 3, suit: 'S', point: 3 },
+    { case: 4, suit: 'S', point: 4 },
+    { case: 5, suit: 'S', point: 5 },
+    { case: 6, suit: 'S', point: 6 },
+    { case: 7, suit: 'S', point: 7 },
+    { case: 8, suit: 'S', point: 8 },
+    { case: 9, suit: 'S', point: 9 },
+    { case: 10, suit: 'S', point: 10 },
+    { case: "J", suit: 'S', point: 10 },
+    { case: "Q", suit: 'S', point: 10 },
+    { case: "K", suit: 'S', point: 10 },
 ];
 
-var cardRand = function() {
+var cardRand = function () {
     var rand = Math.floor(Math.random() * deck.length);
     card = deck[rand]
     usedCards.push(card);
     points = card.point;
     var mod = deck.splice(rand, 1);
-    return "Cards/"+card.case+card.suit+".jpg";   
+    return "Cards/" + card.case + card.suit + ".jpg";
 }
-var draw = function() {
+var draw = function () {
     pBoard.textContent = "Player:" + " " + pScore + " " + "Draw";
     dBoard.textContent = "Dealer:" + " " + dScore + " " + "Draw";
     hit.disabled = true;
     stand.disabled = true;
 }
-var pwin = function() {
+var pwin = function () {
     pBoard.textContent = "Player:" + " " + pScore + " " + "Player Wins";
     hit.disabled = true;
     stand.disabled = true;
 }
-var plose =function() {
+var plose = function () {
     pBoard.textContent = "Player -" + " " + pScore + " " + "Player Busts";
     hit.disabled = true;
     stand.disabled = true;
 }
-var dwin = function() {
+var dwin = function () {
     dBoard.textContent = "Dealer -" + " " + dScore + " " + "Dealer Wins";
     hit.disabled = true;
     stand.disabled = true;
 }
-var dlose = function() {
+var dlose = function () {
     dBoard.textContent = "Dealer -" + " " + dScore + " " + "Dealer Busts";
     hit.disabled = true;
     stand.disabled = true;
 }
-var reset = function() {
-    usedCards.map(function(e) {
+var reset = function () {
+    usedCards.map(function (e) {
         var x = usedCards.splice(e, usedCards.length);
         for (i = 0; i < x.length; i++) {
             deck.push(x[i]);
         }
     })
-    
+
 
 
     while (pHand.firstChild) {
@@ -119,9 +223,9 @@ var reset = function() {
     while (dHand.firstChild) {
         dHand.removeChild(dHand.firstChild);
     }
-    
+
     pScore = 0;
-    dScore = 0; 
+    dScore = 0;
     pBoard.textContent = "Player -";
     dBoard.textContent = "Dealer -";
 
@@ -130,7 +234,7 @@ var reset = function() {
 hit.disabled = true;
 stand.disabled = true;
 
-deal.addEventListener('click', function(){          // Deal Button
+deal.addEventListener('click', function () {          // Deal Button
     hit.disabled = false;
     stand.disabled = false;
 
@@ -138,7 +242,7 @@ deal.addEventListener('click', function(){          // Deal Button
     img.src = cardRand();
     pHand.appendChild(img);
     pScore += points;
-    
+
     var img = document.createElement('img');
     img.src = cardRand();
     pHand.appendChild(img);
@@ -148,7 +252,7 @@ deal.addEventListener('click', function(){          // Deal Button
     img.src = cardRand();
     dHand.appendChild(img);
     dScore += points;
-    
+
     var img = document.createElement('img');
     img.src = cardRand();
     dHand.appendChild(img);
@@ -170,14 +274,14 @@ deal.addEventListener('click', function(){          // Deal Button
 
 });
 
-hit.addEventListener('click', function() {          // Hit Button
+hit.addEventListener('click', function () {          // Hit Button
     var img = document.createElement('img');
     img.src = cardRand();
     pHand.appendChild(img);
     pScore += points;
 
     pBoard.textContent = "Player -" + " " + pScore;
-    
+
     if (pScore > 21) {
         plose();
         hit.disabled = true;
@@ -186,12 +290,12 @@ hit.addEventListener('click', function() {          // Hit Button
         pwin();
         hit.disabled = true;
     }
-    
+
 })
 
-stand.addEventListener('click', function() {        // Stand Button
+stand.addEventListener('click', function () {        // Stand Button
     hit.disabled = true;
-    
+
     while (dScore < 17) {
         var img = document.createElement('img');
         img.src = cardRand();
@@ -213,17 +317,17 @@ stand.addEventListener('click', function() {        // Stand Button
     if (dScore == pScore) {
         draw();
     }
-     else if (dScore > pScore && dScore < 21) {
+    else if (dScore > pScore && dScore < 21) {
         dwin();
     }
-     else if (dScore < pScore && dScore < 21) {
+    else if (dScore < pScore && dScore < 21) {
         pwin();
     }
 })
 
-again.addEventListener('click', function() {        // Again Button
+again.addEventListener('click', function () {        // Again Button
     reset();
     deal.disabled = false;
     hit.disabled = true;
-    stand.disabled = true;  
+    stand.disabled = true;
 })
